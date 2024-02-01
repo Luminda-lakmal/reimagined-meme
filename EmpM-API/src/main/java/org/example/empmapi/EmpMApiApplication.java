63358abcce5ceeee8f63cf4109f71c4030d2a105
Luminda-lakmal <luckmald98@gmail.com>
2024-02-01 16:48:39 +0530

    first commit

---
+++ EmpM-API/src/main/java/org/example/empmapi/EmpMApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.empmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpMApiApplication.class, args);
+    }
+
+}
